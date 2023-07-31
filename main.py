6384e7714ae5b9e171aa5536839be58b3b6ca69b
Monazir Muhammad Doha <70005698+Itsmmdoha@users.noreply.github.com>
2023-07-31 12:03:39 +0600

    adding main.py file
    This file contains both the class ( that will be moved to a assist.py file in future)  and the code that uses the class (will be kept in the main.py file in future).

---
+++ main.py
@@ -0,0 +1,26 @@
+from requests import get
+from json import loads
+
+class Doppelgang:
+    def __init__(self,url,maskingDomain="google.com",keyword="login"):
+        self.url = url
+        self.maskingDomain = maskingDomain
+        self.keyword = keyword
+        self.shorturl = None
+    def shorten(self):
+        res = get(f"https://is.gd/create.php?format=json&url={self.url}").text
+        json = loads(res)
+        self.shorturl = json["shorturl"]
+
+    def mask(self):
+        maskedURL = self.shorturl.replace("https://",f"https://{self.maskingDomain}-{self.keyword}@")
+        return maskedURL
+
+
+if __name__ == "__main__":
+    url = input("Enter the url to be masked: ")
+    mask = input("Enter a masking domain: ")
+    keyword = input("Type a kewword (without whitespace or / ): ")
+    target = Doppelgang(url,mask,keyword)
+    target.shorten()
+    print(target.mask())
