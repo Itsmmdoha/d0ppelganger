2133275df57b49173f7f6d16e6d0110407e6f12e
itsmmdoha <getmonazirnow@gmail.com>
2023-08-13 19:42:49 +0600

    adding colors and banner

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from assist import Doppelgang
-url = input("Enter the url to be masked: ")
+def banner():
+    banner = """\033[92;1m
+    ██████   ██████  ██████  ██████  ███████ ██       ██████   █████  ███    ██  ██████  ███████ ██████  
+    ██   ██ ██  ████ ██   ██ ██   ██ ██      ██      ██       ██   ██ ████   ██ ██       ██      ██   ██ 
+    ██   ██ ██ ██ ██ ██████  ██████  █████   ██      ██   ███ ███████ ██ ██  ██ ██   ███ █████   ██████  
+    ██   ██ ████  ██ ██      ██      ██      ██      ██    ██ ██   ██ ██  ██ ██ ██    ██ ██      ██   ██ 
+    ██████   ██████  ██      ██      ███████ ███████  ██████  ██   ██ ██   ████  ██████  ███████ ██   ██ 
+
+                                                                               by itsmmdoha aka HoundSec                                                                                                    
+    \033[0m"""
+    print(banner)
+banner()
+
+url = input("\033[93mEnter the url to be masked: ")
 mask = input("Enter a masking domain: ")
 keyword = input("Type a kewword (without whitespace or / ): ")
 target = Doppelgang(url,mask,keyword)
 target.shorten()
-print("Output url:",target.mask())
+print("Output url: \033[91;1m",target.mask(),"\033[0m")
