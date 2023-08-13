0a7319d8e0c7852774998673d391a7300b6e36d3
itsmmdoha <getmonazirnow@gmail.com>
2023-08-13 19:56:56 +0600

    fixing typos in prints

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ banner()
 try:
     get("https://google.com")
 except:
-    exit("\033[91;1mGet an internet connection first; you morone!!!\033[0m")
+    exit("\033[91;1mGet an internet connection first; you moron!!!\033[0m \n")
 
 url = input("\033[93mEnter the url to be masked: ")
 mask = input("Enter a masking domain: ")
