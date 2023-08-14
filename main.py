17bca3a8c7d144c3d3c2190ea2206126642c003f
itsmmdoha <getmonazirnow@gmail.com>
2023-08-14 13:38:13 +0600

    removed the banner and the internet test block; they seemed like a bloat after a while

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 from requests import get
 from assist import Doppelgang
-def banner():
-    banner = """\033[92;1m
-    ██████   ██████  ██████  ██████  ███████ ██       ██████   █████  ███    ██  ██████  ███████ ██████  
-    ██   ██ ██  ████ ██   ██ ██   ██ ██      ██      ██       ██   ██ ████   ██ ██       ██      ██   ██ 
-    ██   ██ ██ ██ ██ ██████  ██████  █████   ██      ██   ███ ███████ ██ ██  ██ ██   ███ █████   ██████  
-    ██   ██ ████  ██ ██      ██      ██      ██      ██    ██ ██   ██ ██  ██ ██ ██    ██ ██      ██   ██ 
-    ██████   ██████  ██      ██      ███████ ███████  ██████  ██   ██ ██   ████  ██████  ███████ ██   ██ 
-
-                                                                               by itsmmdoha aka HoundSec                                                                                                    
-    \033[0m"""
-    print(banner)
-banner()
-
-try:
-    get("https://google.com")
-except:
-    exit("\033[91;1mGet an internet connection first; you moron!!!\033[0m \n")
 
 url = input("\033[93mEnter the url to be masked: ")
 mask = input("Enter a masking domain: ")
